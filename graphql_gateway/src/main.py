e4db8a77b4739e15bb77cabd622c642a56f487c1
Luigi Ferreira <luigi.vf@gmail.com>
2025-06-20 23:51:12 -0300

    Api GraphQL completa

--- graphql_gateway/src/main.py
+++ graphql_gateway/src/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 type_defs = load_schema_from_path("schema.graphql")
 
 query = QueryType()
-#mutation = MutationType()
+mutation = MutationType()
 
 @query.field("contatos")
 def resolve_contatos(_, info):
@@ -22,7 +22,73 @@ def resolve_contatos(_, info):
     except requests.RequestException as e:
         raise e
 
-schema = make_executable_schema(type_defs, [query])#, mutation])
+@query.field("contato")
+def resolve_contato(_, info, nome: str):
+    try:
+        response = requests.get(f"http://api_contato:8004/contato/{nome}")
+        return response.json() 
+    except requests.RequestException as e:
+        raise e
+
+@mutation.field("createContato")
+def resolve_create_contato(_, info, input):
+
+    try:
+        response = requests.post(
+            "http://api_contato:8004/contato",
+            json=input
+        )
+        response.raise_for_status()
+
+        return{
+            "message": "Contato criado com sucesso",
+            "contato": input
+        }
+    except requests.RequestException as e:
+        contato_vazio = {
+            "nome": "",
+            "categoria": "",
+            "telefones": [{
+                "numero": "",
+                "tipo": ""
+            }]
+        }
+        return{
+            "message": f"Falha ao criar o contato: {str(e)}",
+            "contato": contato_vazio
+            }
+
+@mutation.field("fillListaDeContatos")
+def resolve_fill_lista_de_contatos(_, info):
+    try:
+        response = requests.post('http://api_manipulation:8005/criar_lista_de_contatos')
+        response.raise_for_status()
+
+        response = requests.get('http://api_manipulation:8005/listar_contatos')
+        response.raise_for_status()
+
+        return{
+            "message": "Lista de contatos criada com sucesso!",
+            "contato": response.json()
+        }
+    except requests.RequestException as e:
+        contato_vazio = {
+            "nome": "",
+            "categoria": "",
+            "telefones": [{
+                "numero": "",
+                "tipo": ""
+            }]
+        }
+
+        contatos = []
+        contatos.append(contato_vazio)
+        return{
+            "message": "deu problema",
+            "contato": contatos
+        }
+
+schema = make_executable_schema(type_defs, [query, mutation])
 
 # Adiciona o Endpoint GraphQL ao FastAPI 
 app.mount("/graphql", GraphQL(schema, debug=True)) # debug=True para habilitar o GraphQL IDE
